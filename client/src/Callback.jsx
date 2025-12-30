dc4123784b0db68e933f383c18b1bdc90e778f90
srikanth <srikanthchowdary8499@gmail.com>
2025-12-30 14:54:54 +0530

    changes done

--- client/src/Callback.jsx
+++ client/src/Callback.jsx
@@ -11,52 +11,6 @@ export default function Callback() {
   useEffect(() => {
     const code = new URLSearchParams(window.location.search).get("code");
 
-    // if (!code) {
-    //   // code already used or missing, redirect to home
-    //   navigate("/home");
-    //   return;
-    // }
-
-    // if(!accessToken){
-    //   fetch(`http://127.0.0.1:3000/callback?code=${code}`)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     console.log(data);
-    //     setAccessToken(data.access_token);
-    //     localStorage.setItem("access_token", data.access_token);
-    //     localStorage.setItem("refresh_token", data.refresh_token);
-
-    //     // Clean the URL so ?code=XXXX is removed
-    //     window.history.replaceState({}, document.title, "/home");
-
-    //     navigate("/home");
-    //   });
-    // }
-
-    // async function getTokens() {
-    //   try {
-    //     const res = await fetch(`http://127.0.0.1:3000/callback?code=${code}`);
-    //     const data = await res.json();
-
-    //     if (!data.access_token) {
-    //       navigate("/");
-    //       return;
-    //     }
-
-    //     localStorage.setItem("access_token", data.access_token);
-    //     localStorage.setItem("refresh_token", data.refresh_token);
-
-    //     // remove ?code from URL
-    //     window.history.replaceState({}, document.title, "/home");
-
-    //     navigate("/home");
-    //   } catch (err) {
-    //     console.error(err);
-    //     navigate("/");
-    //   }
-    // }
-
-    // getTokens();
 
      if (code) {
       fetch(`https://spotify-clone-server-veda.onrender.com/callback?code=${code}`)
